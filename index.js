9a6e07a0bed65dcd2e4f79e9a1e674683d5d4940
Tero Leppänen <tero.leppanen@live.fi>
2023-05-15 11:21:15 +0300

    Add mobileNav-element and make final functionality for mobile drawer.

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const hamburger = document.querySelector(".hamburger");
 const bar1 = document.querySelector(".bar1");
 const bar2 = document.querySelector(".bar2");
 const bar3 = document.querySelector(".bar3");
+const mobileNav = document.querySelector(".mobileNav");
 
 hamburger.addEventListener("click", () => {
+  // animate hamburger bars
   bar1.classList.toggle("animateBar1");
   bar2.classList.toggle("animateBar2");
   bar3.classList.toggle("animateBar3");
+  // open mobile navi
+  mobileNav.classList.toggle("openDrawer");
 });
